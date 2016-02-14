3b67093e4ecad4decaf5e411cd541bc3d83e7e06
Ioana Popa <ioanaandreea476@gmail.com>
2016-02-14 18:45:45 +0200

    Update Social-Network.sql

--- Social-Network.sql
+++ Social-Network.sql
@@ -30,3 +30,11 @@ on Q.ID = Reverse.ID1
 Pair.ID2 = Reverse.ID1
 and Pair.ID1 > Reverse.ID1;
 
+--Q4ind all students who do not appear in the Likes table (as a student who likes or is liked) and return their names and grades. 
+--Sort by grade, then by name within each grade. 
+select name, grade from Highschooler
+where ID not in (select ID1 from Likes)
+and ID not in (select ID2 from Likes)
+order by grade, name;
+
+
